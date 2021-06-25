d4d6bbf56a04cb833ca01fe6e6bfed06faa924f3
bpurkuti <bishwopurkutihhs@gmail.com>
2021-06-25 09:41:29 -0700

    testing fork

--- src/main/test/java/dev/marker/daotests/RoutineDaoTests.java
+++ src/main/test/java/dev/marker/daotests/RoutineDaoTests.java
@@ -8,7 +8,7 @@ public class RoutineDaoTests {
 
     static RoutineDao routineDao = null;
 
-    //Not sure about routineId being a string
+    //Not sure about routineId being a string||Change to Int?
     static Routine testRoutine = new Routine("0", "Mack", "Legs Day", 0, 0);
 
 }
