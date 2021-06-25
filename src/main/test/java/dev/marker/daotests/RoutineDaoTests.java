c2ebcef46ad65f166ef9f4d8b3669e1f514797fc
bpurkuti <bishwopurkutihhs@gmail.com>
2021-06-25 08:59:54 -0700

    Updated with main branch+ Started RoutineDaoTests

---
+++ src/main/test/java/dev/marker/daotests/RoutineDaoTests.java
@@ -0,0 +1,11 @@
+package dev.marker.daotests;
+
+import daos.RoutineDao;
+import entities.Routine;
+
+public class RoutineDaoTests {
+
+    static RoutineDao routineDao = null;
+    //Not sure about routineId being a string
+    static Routine testRoutine = new Routine("0", "Mack", "Legs Day", 0, 0);
+}
